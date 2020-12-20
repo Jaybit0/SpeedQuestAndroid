a6abcce525adc33d05c5c0178cd84a81f3c3577d
AndreasSigi <andreas-sigl@gmx.at>
2020-12-20 14:22:47 +0100

    Bugfix TapColorNotWord

--- app/src/main/java/de/dhbw/apps/speedquest/game/handlers/TapColorNotWordHandler.java
+++ app/src/main/java/de/dhbw/apps/speedquest/game/handlers/TapColorNotWordHandler.java
@@ -106,6 +106,8 @@ public class TapColorNotWordHandler extends GameHandler {
             return;
         }
 
+        onUpdateColor();
+
         startMillis = System.currentTimeMillis();
         failedClicks = 0;
         failedText.setText(failedClicks + "");
